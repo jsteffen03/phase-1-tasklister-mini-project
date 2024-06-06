3825c78a14c041662871fe7409a09e85fd6567c2
Justin Steffen <jcsteffen03@gmail.com>
2024-06-06 11:54:11 -0600

    lab Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  const clickSubmit = document.querySelector("#create-task-form")
+  console.log(clickSubmit)
+    clickSubmit.addEventListener("submit", (event) => {
+      event.preventDefault()
+      console.log(event.target); // debug
+        const newTask = {task:event.target["new-task-description"].value,}
+        console.log(newTask)
+        displayTask(newTask)
+    })
+
+
+function displayTask(snow){
+  const task_list = document.querySelector("#tasks")
+  const task = document.createElement("h2")
+
+  task.textContent = snow.task
+
+
+const div = document.createElement("li")
+  div.append(task)
+  task_list.append(div)
+
+
+}
+
+
+
+})
